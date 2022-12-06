d384036395f733fd23323a082f1b96bca96f2cba
Crowdbotics <team@crowdbotics.com>
2022-12-06 00:31:40 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/daniel_fitness_app_37806/MainActivity.java
@@ -0,0 +1,15 @@
+package com.daniel_fitness_app_37806;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "daniel_fitness_app_37806";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "daniel_fitness_app_37806",
+  url: "https://daniel_fitness_app_37806.botics.co",
+  api: "https://daniel_fitness_app_37806.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
